0b35568397035e01a3a73e7207495bc98d1c8171
Walker Fisher <walkerfisher@walkers-air.mynetworksettings.com>
2024-08-18 17:24:22 -0700

    event component created

---
+++ src/events/EventComponent.tsx
@@ -0,0 +1,14 @@
+const EventComponent: React.FC = () => {
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+
+  return (
+    <div>
+      <input onChange={onChange} />
+    </div>
+  )
+};
+
+export default EventComponent;
